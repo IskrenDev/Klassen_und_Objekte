5874b1abb167a75088746741cb8878912d2d2dd9
iskrenr <iskrenr005@gmail.com>
2023-09-26 15:56:56 +0200

    Schritte 1 bis 5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Book book1 = new Book("Java Basics", "Iskren Radev", "12345678");
+        Book book2 = new Book("Java Advanced", "Fabian Link", "87654321");
+
+        Book[] books = {book1, book2};
+
+        Library library1 = new Library(books);
+
+        System.out.println(library1);
     }
+
 }
